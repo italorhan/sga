f1b1295e72d6ebff874c954da544e03384097604
italorhan <italorhan@gmail.com>
2024-03-01 16:27:17 -0300

    criacao tabela fornecedor

--- sga/admin.py
+++ sga/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from sga.models import Licenca
+from sga.models import Licenca, Fornecedor
+
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
 
 class LicencaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'fornecedor', 'licenca', 'quantidade', 'data_expiracao')
     search_fields = ('nome', 'fornecedor')
 
+admin.site.register(Fornecedor, FornecedorAdmin)
 admin.site.register(Licenca, LicencaAdmin)
--- sga/models.py
+++ sga/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
+class Fornecedor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+    
 class Licenca(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200, blank=False, null=False)
-    fornecedor = models.CharField(max_length=200, blank=True, null=True)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.PROTECT, related_name='licenca_fornecedor')
     licenca = models.CharField(max_length=200, blank=True, null=True)
     quantidade = models.IntegerField(blank=False, null=False)
     quantidade_utilizada = models.IntegerField(blank=True, null=True)
     quantidade_disponivel = models.IntegerField(blank=True, null=True)
-    data_aquisicao = models.DateField(blank=False, null=False)
+    data_aquisicao = models.DateField(blank=True, null=True)
     data_expiracao = models.DateField(blank=True, null=True)
     valor_unitario = models.FloatField(blank=False, null=False)
 
