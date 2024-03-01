4ffb4721f82eba81ffe6543238a825930f4fb193
italorhan <italorhan@gmail.com>
2024-02-29 21:43:40 -0300

    cadastro inicial licenças

--- sga/admin.py
+++ sga/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from sga.models import Licenca
 
-# Register your models here.
+class LicencaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'fornecedor', 'licenca', 'quantidade', 'data_expiracao')
+    search_fields = ('nome', 'fornecedor')
+
+admin.site.register(Licenca, LicencaAdmin)
--- sga/models.py
+++ sga/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Licenca(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200, blank=False, null=False)
+    fornecedor = models.CharField(max_length=200, blank=True, null=True)
+    licenca = models.CharField(max_length=200, blank=True, null=True)
+    quantidade = models.IntegerField(blank=False, null=False)
+    data_aquisicao = models.DateField(blank=False, null=False)
+    data_expiracao = models.DateField(blank=True, null=True)
+    valor_unitario = models.FloatField(blank=False, null=False)
+
+    def __str__(self):
+        return self.nome
